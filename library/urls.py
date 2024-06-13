53e4b4fc60c77c530cdb20a0adedd10cdd54b7dd
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-13 19:08:03 +0200

    feat: set up book routes

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from library.views import BookViewSet
 
+
+router = DefaultRouter()
+router.register("books", BookViewSet)
 
 urlpatterns = [
-
+    path('', include(router.urls)),
 ]
 
 app_name = "library"
