12158e9a0e7d8b8d624873e5934e4e8950364861
Andrii <andreboiko1@gmail.com>
2024-06-13 22:35:03 +0300

    fix with flake8

--- user/urls.py
+++ user/urls.py
@@ -9,9 +9,9 @@ from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
 app_name = "user"
