d55beb6e019e51c36af6d3e8b03a3556e9068e2b
Andrii <andreboiko1@gmail.com>
2024-06-13 22:35:03 +0300

    fix with flake8

--- user/admin.py
+++ user/admin.py
@@ -28,11 +28,29 @@ class UserAdmin(DjangoUserAdmin):
         (
             None,
             {
-                "classes": ("wide",),
-                "fields": ("email", "password1", "password2"),
+                "classes": (
+                    "wide",
+                ),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                ),
             },
         ),
     )
-    list_display = ("email", "first_name", "last_name", "is_staff", "is_active")
-    search_fields = ("email", "first_name", "last_name")
-    ordering = ("email",)
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active"
+    )
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name"
+    )
+    ordering = (
+        "email",
+    )
