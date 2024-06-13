24da570aa5ad4a31972113c84b6454041d533e2e
Shershen Alexandra <hornet240204@gmail.com>
2024-06-13 19:20:10 +0300

    Add project apps to the INSTALLED_APPS

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
+    'borrowing',
+    'payment',
+    'user',
 ]
 
 MIDDLEWARE = [
