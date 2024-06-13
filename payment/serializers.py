fb7c285f31b4345249ec4ec5c644f41c9180adfc
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-13 21:16:09 +0200

    feat: add viewset for Payment

--- payment/serializers.py
+++ payment/serializers.py
@@ -10,13 +10,15 @@ class PaymentSerializer(serializers.ModelSerializer):
             "borrowing",
             "status",
             "type",
+            "session_url",
+            "session_id",
             "money_to_pay",
             "created_at",
             "updated_at",
         )
 
 
-class PaymentDetailSerializer(serializers.ModelSerializer):
+class PaymentListSerializer(serializers.ModelSerializer):
     borrowing = serializers.CharField(source="borrowing.id", read_only=True)
     book = serializers.CharField(source="borrowing.book.id", read_only=True)
     user = serializers.CharField(source="borrowing.user.email", read_only=True)
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from payment.models import Payment
+from payment.serializers import PaymentSerializer, PaymentListSerializer
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Payment.objects.select_related("borrowing__book", "borrowing__user")
+
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "list"]:
+            return PaymentListSerializer
+        return PaymentSerializer
