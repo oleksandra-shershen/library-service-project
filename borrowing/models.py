ba9deaacb5ea905a1bc435c6b20de768bf9c2761
Shershen Alexandra <hornet240204@gmail.com>
2024-06-13 20:22:02 +0300

    Add migrations and models. Register model in admin

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,28 @@
+from datetime import date
+
 from django.db import models
+
+from library.models import Book
+from library_service import settings
 
 
 class Borrowing(models.Model):
-    pass
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings",
+    )
+
+    def __str__(self) -> str:
+        return (
+            f"{self.book}: from {self.borrow_date} "
+            f"to {self.expected_return_date}."
+        )
