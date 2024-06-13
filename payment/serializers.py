317f56e3b9e07c725e360dfd29f58c8b601424b6
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-13 21:08:01 +0200

    feat: add serializers for Payment model

---
+++ payment/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Payment
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "borrowing",
+            "status",
+            "type",
+            "money_to_pay",
+            "created_at",
+            "updated_at",
+        )
+
+
+class PaymentDetailSerializer(serializers.ModelSerializer):
+    borrowing = serializers.CharField(source="borrowing.id", read_only=True)
+    book = serializers.CharField(source="borrowing.book.id", read_only=True)
+    user = serializers.CharField(source="borrowing.user.email", read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = (
+            "id",
+            "borrowing",
+            "book",
+            "user",
+            "status",
+            "type",
+            "money_to_pay",
+            "created_at",
+            "updated_at",
+        )
