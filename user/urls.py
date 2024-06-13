164ed82d590c4f6a53dea119efb25cf120cbd55c
Andrii <andreboiko1@gmail.com>
2024-06-13 21:44:13 +0300

    added urls for user

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="create"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
 ]
-
 app_name = "user"
