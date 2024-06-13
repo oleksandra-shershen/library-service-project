2a207537ac94915e88bd0e470c58a7af547f1a6e
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-13 21:22:54 +0200

    feat: add routers and test package

--- payment/urls.py
+++ payment/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from payment.views import PaymentViewSet
 
+
+router = DefaultRouter()
+router.register("payment", PaymentViewSet, basename="payment")
 
 urlpatterns = [
-
+    path("", include(router.urls)),
 ]
 
 app_name = "payment"
