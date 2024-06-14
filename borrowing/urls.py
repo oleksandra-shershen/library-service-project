747cbf08ef401365bbe67ef8a1ee122529d34d75
Vitalii <vitalikbashkiser@gmail.com>
2024-06-14 23:01:48 +0300

    Cuple edits in another files

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from borrowing.views import BorrowingViewSet
+from borrowing.views import BorrowViewSet
 
 router = DefaultRouter()
-router.register("borrowings", BorrowingViewSet, basename="borrowing")
+router.register("borrowings", BorrowViewSet, basename="borrowing")
 
 urlpatterns = [
     path("", include(router.urls)),
