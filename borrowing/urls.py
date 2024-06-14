6b71b79aff60c4e682be0f7ab6b36fe48e888b88
Vitalii <vitalikbashkiser@gmail.com>
2024-06-14 23:01:48 +0300

    Cuple edits in another files

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BorrowViewSet
+from borrowing.views import BorrowViewSet
 
 router = DefaultRouter()
 router.register("borrowings", BorrowViewSet, basename="borrowing")
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,20 +1,17 @@
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 from borrowing.models import Borrowing
 from borrowing.serializers import (
     BorrowingSerializer,
     BorrowingListSerializer,
     BorrowingDetailSerializer,
 )
+from rest_framework.permissions import IsAuthenticated
 
 
-class BorrowViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet,
-):
+class BorrowViewSet(viewsets.ModelViewSet):
     queryset = Borrowing.objects.all().select_related("book", "user")
     serializer_class = BorrowingSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
         if self.action == "list":
@@ -22,3 +19,21 @@ class BorrowViewSet(
         elif self.action == "retrieve":
             return BorrowingDetailSerializer
         return self.serializer_class
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            user_id = self.request.query_params.get("user_id")
+            if user_id:
+                queryset = queryset.filter(user_id=user_id)
+        else:
+            queryset = queryset.filter(user=self.request.user)
+
+        is_active = self.request.query_params.get("is_active")
+        if is_active:
+            if is_active.lower() == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+            elif is_active.lower() == "false":
+                queryset = queryset.exclude(actual_return_date__isnull=True)
+
+        return queryset
