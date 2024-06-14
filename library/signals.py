544ebcbf0c6919cb3b27525be43d18724eb02b2e
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-14 18:28:48 +0200

    updt: special signals for email users

--- library/signals.py
+++ library/signals.py
@@ -3,9 +3,10 @@ import requests
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Book
+from user.models import User
+
 
 TELEGRAM_BOT_TOKEN = os.environ.get("TOKEN")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 
 TELEGRAM_API_URL = (
     f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
@@ -15,12 +16,13 @@ TELEGRAM_API_URL = (
 @receiver(post_save, sender=Book)
 def send_telegram_notification(sender, instance, created, **kwargs):
     if created:
+        users = User.objects.exclude(telegram_chat_id__isnull=True)
         message = f"New book added: {instance.title} by {instance.author}. Price: ${instance.daily_fee}"
-        print(
-            f"Sending message to {TELEGRAM_CHAT_ID}: {message}"
-        )
-        response = requests.post(
-            TELEGRAM_API_URL,
-            data={"chat_id": TELEGRAM_CHAT_ID, "text": message},
-        )
-        print(response.json())
+        for user in users:
+            chat_id = user.telegram_chat_id
+            if chat_id:
+                response = requests.post(
+                    TELEGRAM_API_URL,
+                    data={"chat_id": chat_id, "text": message},
+                )
+                print(response.json())
