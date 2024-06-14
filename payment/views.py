ef095eca1831c0fc22d2265b557ac7f3586584c5
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-14 12:32:56 +0200

    feat: add permissions for payment

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from payment.models import Payment
 from payment.serializers import (
@@ -15,8 +16,21 @@ class PaymentViewSet(
     mixins.UpdateModelMixin,
     viewsets.GenericViewSet,
 ):
-    queryset = Payment.objects.all().select_related("borrowing__book", "borrowing__user")
+    queryset = Payment.objects.all().select_related(
+        "borrowing__book", "borrowing__user"
+    )
     serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Payment.objects.all().select_related(
+                "borrowing__book", "borrowing__user"
+            )
+        return Payment.objects.filter(borrowing__user=user).select_related(
+            "borrowing__book", "borrowing__user"
+        )
 
     def get_serializer_class(self):
         if self.action == "list":
