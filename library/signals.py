e40bdddf99e51a71a4cd6db16d411fb51a587557
Vitalii <vitalikbashkiser@gmail.com>
2024-06-14 23:26:34 +0300

    flake8 resolve

--- library/signals.py
+++ library/signals.py
@@ -17,8 +17,10 @@ TELEGRAM_API_URL = (
 def send_telegram_notification(sender, instance, created, **kwargs):
     if created:
         users = User.objects.exclude(telegram_chat_id__isnull=True)
-        message = (f"New book added: {instance.title} by {instance.author}. "
-                   f"Price: ${instance.daily_fee}")
+        message = (
+            f"New book added: {instance.title} by {instance.author}. "
+            f"Price: ${instance.daily_fee}"
+        )
         for user in users:
             chat_id = user.telegram_chat_id
             if chat_id:
