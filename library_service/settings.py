9c3d1354d0f0d8432e74d9affa7f8e4b13c4d462
Shershen Alexandra <hornet240204@gmail.com>
2024-06-14 22:31:50 +0300

    Change postgres to the db naming

--- library_service/settings.py
+++ library_service/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         "NAME": os.environ.get('POSTGRES_DB'),
         "USER": os.environ.get('POSTGRES_USER'),
         "PASSWORD": os.environ.get('POSTGRES_PASSWORD'),
-        "HOST": os.environ.get('POSTGRES_HOST', 'postgres'),
+        "HOST": os.environ.get('POSTGRES_HOST', 'db'),
         "PORT": os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
