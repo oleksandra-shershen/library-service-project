4185feeafc1c65c640b5dd70f32f816d3ee79fee
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-14 18:23:26 +0200

    feat: add email validation

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -1,8 +1,15 @@
 import logging
 import os
 
+import django
+from django.contrib.auth import get_user_model
 from telegram import Update
 from telegram.ext import ContextTypes, ApplicationBuilder, CommandHandler
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "library_service.settings")
+django.setup()
+User = get_user_model()
+
 
 TOKEN = os.environ.get("TOKEN")
 
@@ -14,22 +21,25 @@ logger = logging.getLogger(__name__)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Siemano!")
+    await update.message.reply_text("Hello! Please send me your email to receive notifications.")
 
 
-async def notify_new_book(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    book_title = context.args[0] if context.args else "Unknown"
-    author = context.args[1] if context.args > 1 else "Unknown"
-    price = context.args[2] if context.args > 2 else "Unknown"
-    await update.message.reply_text(
-        f"New book added: {book_title} by {author}. Price: ${price}"
-    )
+async def get_email(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    email = update.message.text
+    chat_id = update.message.chat_id
+    user=User.objects.filter(email=email).first()
+    if user:
+        user.telegram_chat_id = chat_id
+        user.save()
+        await update.message.reply_text("Thank you! Your chat ID has been saved.")
+    else:
+        await update.message.reply_text("Sorry, I couldn't find that email.")
 
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("notify_new_book", notify_new_book))
+    application.add_handler(CommandHandler("notify_new_book", get_email))
 
     application.run_polling()
