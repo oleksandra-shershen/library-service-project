e8fda513238f27235c222cd98dd6c47143e1a38c
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-14 09:19:17 +0200

    fix: fix setting conflict

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # External apps
+    'rest_framework_simplejwt',
     'rest_framework',
+
+    # User apps
     'library',
     'borrowing',
     'payment',
