7738e01fb9297237ad7591e46b91cb73b7d035c6
Dmytro Pukas <pukasdima2002@gmail.com>
2024-06-15 11:39:31 +0200

    feat: send new borrow notification

--- library/signals.py
+++ library/signals.py
@@ -2,25 +2,21 @@ import os
 import requests
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from library.models import Book
+from django_q.tasks import async_task
+from borrowing.models import Borrowing
+from .models import Book
 from user.models import User
 
+
 TELEGRAM_BOT_TOKEN = os.environ.get("TOKEN")
+
 TELEGRAM_API_URL = (
     f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
 )
 
 
-def send_telegram_message(chat_id, message):
-    response = requests.post(
-        TELEGRAM_API_URL,
-        data={"chat_id": chat_id, "text": message},
-    )
-    print(response.json())
-
-
 @receiver(post_save, sender=Book)
-def send_telegram_notification(sender, instance, created, **kwargs):
+def send_telegram_notification(instance, created):
     if created:
         users = User.objects.exclude(telegram_chat_id__isnull=True)
         message = (
@@ -35,3 +31,26 @@ def send_telegram_notification(sender, instance, created, **kwargs):
                     data={"chat_id": chat_id, "text": message},
                 )
                 print(response.json())
+
+
+def send_borrowing_notification(instance, created):
+    if created:
+        user = instance.user
+        message = (
+            f"New borrowing created:\n"
+            f"Book: {instance.book.title}\n"
+            f"Author: {instance.book.author}\n"
+            f"Due date: {instance.expected_return_date}\n"
+        )
+        if user.telegram_chat_id:
+            response = requests.post(
+                TELEGRAM_API_URL,
+                data={"chat_id": user.telegram_chat_id, "text": message},
+            )
+            print(response.json())
+
+
+@receiver(post_save, sender=Borrowing)
+def handle_new_borrowing(sender, instance, created, **kwargs):
+    if created:
+        async_task(send_borrowing_notification, instance, created)
