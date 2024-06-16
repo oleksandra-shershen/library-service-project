8dea3bbc1de0d1e289878ef15fb916198620874d
Andrii <andreboiko1@gmail.com>
2024-06-16 10:47:49 +0300

    added func for calculate_total_price for book added func for stripe session

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,10 +1,11 @@
-from datetime import timezone
+from datetime import timezone, datetime
+import stripe
 
 from django.db import models
 from rest_framework.exceptions import ValidationError
+from django.conf import settings
 
 from library.models import Book
-from library_service import settings
 
 
 class Borrowing(models.Model):
@@ -51,3 +52,43 @@ class Borrowing(models.Model):
         self.book.inventory += 1
         self.book.save()
         self.save()
+
+    def calculate_total_price(self):
+        today = datetime.now().date()
+        days_borrowed = (self.expected_return_date - today).days
+        total_price = self.book.daily_fee * days_borrowed
+        total_price = int(total_price * 100)    # Stripe expects amount in cents
+        return total_price
+
+    def create_stripe_session(self):
+        from payment.models import Payment  # Lazy import
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+
+        total_price = self.calculate_total_price()
+
+        session = stripe.checkout.Session.create(
+            payment_method_types=["card"],
+            line_items=[{
+                "price_data": {
+                    "currency": "usd",
+                    "product_data": {
+                        "name": self.book.title,
+                    },
+                    "unit_amount": total_price,
+                },
+                "quantity": 1,
+            }],
+            mode="payment",
+            success_url="http://localhost:8000/api/payment/completed/" + "?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url="http://localhost:8000/api/payment/canceled/"
+        )
+
+        payment = Payment.objects.create(
+            borrowing=self,
+            session_url=session.url,
+            session_id=session.id,
+            money_to_pay=total_price,
+            status="PENDING",
+        )
+
+        return payment
