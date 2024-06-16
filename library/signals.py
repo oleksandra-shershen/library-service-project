88eb5069f5fc3b724a01b66849c5b20b89059558
Vitalii <vitalikbashkiser@gmail.com>
2024-06-16 20:57:04 +0300

    Add def send_borrowing_notification

--- library/signals.py
+++ library/signals.py
@@ -1,7 +1,11 @@
 import os
 import requests
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_q.tasks import async_task
+
+from borrowing.models import Borrowing
 from library.models import Book
 from user.models import User
 
@@ -27,7 +31,7 @@ def send_telegram_notification(sender, instance, created, **kwargs):
             f"📚 New Book Added!\n\n"
             f"📖 Title: {instance.title}\n"
             f"👤 Author: {instance.author}\n"
-            f"💵 Price per day: ${instance.daily_fee:.2f}\n"
+            f"💵 Price per day: ${float(instance.daily_fee):.2f}\n"
         )
         for user in users:
             chat_id = user.telegram_chat_id
@@ -37,3 +41,26 @@ def send_telegram_notification(sender, instance, created, **kwargs):
                     data={"chat_id": chat_id, "text": message},
                 )
                 print(response.json())
+
+
+def send_borrowing_notification(instance, created):
+    if created:
+        user = instance.user
+        message = (
+            f"New borrowing created:\n"
+            f"Book: {instance.book.title}\n"
+            f"Author: {instance.book.author}\n"
+            f"Due date: {instance.expected_return_date}\n"
+        )
+        if user.telegram_chat_id:
+            response = requests.post(
+                TELEGRAM_API_URL,
+                data={"chat_id": user.telegram_chat_id, "text": message},
+            )
+            print(response.json())
+
+
+@receiver(post_save, sender=Borrowing)
+def handle_new_borrowing(sender, instance, created, **kwargs):
+    if created:
+        async_task(send_borrowing_notification, instance, created)
