f692297028802e1134f48fa28fe983409ee951c5
Andrii <andreboiko1@gmail.com>
2024-06-16 10:50:10 +0300

    added urls for all payment process

--- payment/urls.py
+++ payment/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from payment.views import PaymentViewSet
-
+from payment.views import PaymentViewSet, PaymentCompletedView, PaymentCanceledView, PaymentProcessView
 
 router = DefaultRouter()
 router.register("payment", PaymentViewSet, basename="payment")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("process/", PaymentProcessView.as_view(), name="process"),
+    path("completed/", PaymentCompletedView.as_view(), name="completed"),
+    path("canceled/", PaymentCanceledView.as_view(), name="canceled"),
 ]
 
 app_name = "payment"
