7ebb0a471caa046ada1de0ef9496f1bfb3662dd2
Andrii <andreboiko1@gmail.com>
2024-06-17 09:07:07 +0300

    added spectacular settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -43,10 +43,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
     # External apps
     "rest_framework_simplejwt",
     "rest_framework",
     "django_q",
+    "drf_spectacular",
+
     # User apps
     "library.apps.LibraryConfig",
     "borrowing.apps.BorrowingConfig",
@@ -87,14 +90,20 @@ WSGI_APPLICATION = "library_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ["POSTGRES_DB"],
+#         "USER": os.environ["POSTGRES_USER"],
+#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+#         "HOST": os.environ["POSTGRES_HOST"],
+#         "PORT": os.environ["POSTGRES_PORT"],
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -155,6 +164,7 @@ REST_FRAMEWORK = {
         "rest_framework.throttling.UserRateThrottle",
     ],
     "DEFAULT_THROTTLE_RATES": {"anon": "75/day", "user": "150/day"},
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
@@ -199,4 +209,20 @@ CACHES = {
             f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0"
         ),
     }
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Library Service',
+    'DESCRIPTION': 'This project was created specifically for a library management system. '
+                   'It facilitates the operation of library services, including book borrowings, '
+                   'user management, and payment handling via this API.',
+    'VERSION': '1.0.0',
+    "Author": "Mock team",
+    "SERVICE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
 }
