88e35184d435df3263b62d7ad4bf08b5a978df0c
Andrii <andreboiko1@gmail.com>
2024-06-17 09:07:24 +0300

    added urls for schemas

--- library_service/urls.py
+++ library_service/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,4 +12,7 @@ urlpatterns = [
     path("api/library/", include("library.urls", namespace="library")),
     path("api/payment/", include("payment.urls", namespace="payment")),
     path("api/user/", include("user.urls", namespace="user")),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
