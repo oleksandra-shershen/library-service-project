cad6c7b16d5d5214402ad066ef753fcfa5749b09
Andrii <andreboiko1@gmail.com>
2024-06-17 09:14:48 +0300

    added schema for BookViewSet

---
+++ library/schemas.py
@@ -0,0 +1,18 @@
+from drf_spectacular.utils import (
+    extend_schema,
+)
+
+from library.serializers import BookSerializer
+
+
+class BookSchema:
+    list = extend_schema(
+        responses={
+            200: BookSerializer(many=True),
+        }
+    )
+    retrieve = extend_schema(
+        responses={
+            200: BookSerializer(many=True),
+        }
+    )
