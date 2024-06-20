d3199eb0709fc6f1edd915cf8a7c9954e387eabb
Andrii <andreboiko1@gmail.com>
2024-06-20 17:26:04 +0300

    added settings for ALLOWED_HOSTS and DEBUG

--- library_service/settings.py
+++ library_service/settings.py
@@ -12,11 +12,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", False)
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
 
 # Application definition
 
